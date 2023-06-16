45f07770ac7d93c42a6fc72e0f441923b0b53ead
maxiamikel <maxloversist@gmail.com>
2023-06-16 04:17:03 -0300

    Start project and upload

---
+++ back-app-order/src/main/java/com/maxi/backapporder/BackAppOrderApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.backapporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAppOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAppOrderApplication.class, args);
+	}
+
+}
---
+++ back-app-order/src/test/java/com/maxi/backapporder/BackAppOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.backapporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAppOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
