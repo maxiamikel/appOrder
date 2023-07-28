1afc40cfe552c3858380a8376e42fff2e50d0e3f
maxiamikel <maxloversist@gmail.com>
2023-07-28 10:36:15 -0300

    Implementar a entidade User

---
+++ back-app-order/src/main/java/com/maxi/backapporder/entities/User.java
@@ -0,0 +1,107 @@
+package com.maxi.backapporder.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String matricule;
+    private String telephone;
+    private LocalDate entryDate;
+    public User() {
+    }
+    public User(Long id, String name, String email, String matricule, String telephone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.matricule = matricule;
+        this.telephone = telephone;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMatricule() {
+        return matricule;
+    }
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((telephone == null) ? 0 : telephone.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (telephone == null) {
+            if (other.telephone != null)
+                return false;
+        } else if (!telephone.equals(other.telephone))
+            return false;
+        return true;
+    }
+
+    
+    
+}
